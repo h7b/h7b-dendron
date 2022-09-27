c4ac77534e6a86aa6216a074604983c0d54023de
Huy <github.5sxsf@simplelogin.co>
2022-09-27 15:30:06 +0200

    update vault

---
+++ dendron-publish-site.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# dendron-publish-site.sh
+# This script should exist in the root of your workspace
+
+# Remove docs directory if present
+rm -rf docs
+
+# Uncomment if wishing to remove cache
+# Recommended: Leave cache alone to increase speed of deployments
+#rm -rf .next
+#rm -rf node_modules
+
+# Install latest version of Dendron
+# yarn add @dendronhq/dendron-cli@latest
+# OPTIONALLY
+# Install version of Dendron from yarn.lock in workspace root
+# To use:
+# Uncomment the next line, and comment out the other `yarn install ...` line
+yarn
+
+# Update Dendron Next.js if needed
+(test -d .next) && (echo 'updating dendron next...' && cd .next && git reset --hard && git pull && yarn && cd ..) || (echo 'init dendron next' && yarn dendron publish init)
+
+# Generate static site with nextjs
+yarn dendron publish export
+
+# Move generated website to docs directory in workspace root
+mv .next/out docs
